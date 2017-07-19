8a846b5f91da11c791b45b8085eaa4887352aac1
Jedrzej <stankowski.jedrzej@gmail.com>
2017-07-19 15:57:30 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        System.out.println("dupa");
+        System.out.println("daw");
+        System.out.println("zmiana3");
+        System.out.println("4");
+    }
+}
